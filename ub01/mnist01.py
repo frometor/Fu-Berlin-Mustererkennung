c5b4a3ab021db707d3f7e3d0ad638add53c54d62
Erik Z <etor@gmx.de>
2015-10-31 21:56:33 +0100

    plotting 10 images from data and their target value

--- ub01/mnist01.py
+++ ub01/mnist01.py
@@ -1,3 +1,4 @@
+#%matplotlib inline
 import random
 import sys
 import numpy as np
@@ -6,12 +7,19 @@ from sklearn.datasets import fetch_mldata
 import matplotlib.cm as cm
 customhome = '~/Documents/Mustererkennung/ub01'
 
-mnist = fetch_mldata('MNIST original', data_home= customhome).data
-counter = 1
+mnist_dt = fetch_mldata('MNIST original', data_home= customhome).data
+mnist_tg = fetch_mldata('MNIST original', data_home= customhome).target
+
+print('data.shape: ', mnist_dt.shape)
+print('target.shape: ',mnist_tg.shape)
+
+cnt = 1
 for i in range (1, 3):
     for j in range (1, 6):
-        plt.subplot(3,5, counter)
-        plt.imshow(mnist[(((random.randint(0,9))) * 7000)].reshape((28, 28)),cmap=cm.Greys_r)
+        plt.subplot(2,5, cnt)
+        rand =random.randint(1,70000)
+        print (mnist_tg[rand])
+        plt.imshow(mnist_dt[rand].reshape((28, 28)),cmap=cm.Greys_r)
         plt.axis('off')
-        counter+=1
+        cnt+=1
 plt.show()
