7fc3df896bafa0ddbb65c7549e674db6428d05bc
Florian Hardow <florianhardow@yahoo.de>
2015-11-17 17:49:05 +0100

    all exercises ready

--- ub03/ub03_01.py
+++ ub03/ub03_01.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import scipy.stats as stats
 import math
@@ -7,19 +8,16 @@ import math
 #gets row(of chicken_csv) and a list of (means,sigmas) 
 #and returns the best class for that row
 def returnClass(row,list):
-    counter = 0    
     safewkt=0
     safeclass = 0
-    for x in range(0,5):
-        counter += 1
+    for x in range(0,6):
         wkt = stats.norm.pdf(row[1],meanAndSigma[x][0],meanAndSigma[x][1])
         if (safewkt < wkt):
             safewkt = wkt
             safeclass = x
     return safeclass
  
-
-
+    
 #exercise 1a and b)
 csv = np.genfromtxt('chickwts_training.csv', delimiter=",")
 array0= []
@@ -37,7 +35,7 @@ cnt3=0
 cnt4=0
 cnt5=0
 
-x = np.linspace(100,400,5000)
+x = np.linspace(0,500,5000)
 
 for i in range(len(csv)):
     if (csv[i][2] == 0):
@@ -70,58 +68,66 @@ meanAndSigma = []
 
 print('class0 = red')
 print('Erwartungswert 0: ',np.mean(array0np[:,1]))
-print('Standartabweichung 0: ',np.std(array0np[:,1]))
+print('Varianz 0: ',np.var(array0np[:,1]))
 print('A-Priori 0:',(float(cnt0)/len(csv)))
 mean= np.mean(array0np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array0np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='red')
+red_patch = mpatches.Patch(color='red', label='class1')
 
 print('class1 = blue')
 print('Erwartungswert 1: ',np.mean(array1np[:,1]))
-print('Standartabweichung 1: ',np.std(array1np[:,1]))
+print('Varianz 1: ',np.var(array1np[:,1]))
 print('A-Priori 0:',(float(cnt1)/len(csv)))
 mean= np.mean(array1np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array1np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='b')
+blue_patch = mpatches.Patch(color='blue', label='class2')
 
 print('class2 = green')
 print('Erwartungswert 2: ',np.mean(array2np[:,1]))
-print('Standartabweichung 2: ',np.std(array2np[:,1]))
+print('Varianz 2: ',np.var(array2np[:,1]))
 print('A-Priori 0:',(float(cnt2)/len(csv)))
 mean= np.mean(array2np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array2np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='green')
+green_patch = mpatches.Patch(color='green', label='class3')
 
 print('class3 = black')
 print('Erwartungswert 3: ',np.mean(array3np[:,1]))
-print('Standartabweichung 3: ',np.std(array3np[:,1]))
+print('Varianz 3: ',np.var(array3np[:,1]))
 print('A-Priori 0:',(float(cnt3)/len(csv)))
 mean= np.mean(array3np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array3np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='black')
+black_patch = mpatches.Patch(color='black', label='class4')
 
 print('class4 = yellow')
 print('Erwartungswert 4: ',np.mean(array4np[:,1]))
-print('Standartabweichung 4: ',np.std(array4np[:,1]))
+print('Varianz 4: ',np.var(array4np[:,1]))
 print('A-Priori 0:',(float(cnt4)/len(csv)))
 mean= np.mean(array4np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array4np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='yellow')
+yellow_patch = mpatches.Patch(color='yellow', label='class5')
 
 print('class5 = orange')
 print('Erwartungswert 5:',np.mean(array5np[:,1]))
-print('Standartabweichung 5: ',np.std(array5np[:,1]))
+print('Varianz 5: ',np.var(array5np[:,1]))
 print('A-Priori 0:',(float(cnt5)/len(csv)))
 mean= np.mean(array5np[:,1])
-sigma= math.sqrt(mean)
+sigma= math.sqrt(np.var(array5np[:,1]))
 meanAndSigma.append((mean,sigma))
 plt.plot(x,stats.norm.pdf(x,mean,sigma),c='orange')
+orange_patch = mpatches.Patch(color='orange', label='class6')
 
+
+plt.legend(bbox_to_anchor=(1,1),loc=2,handles=[red_patch,blue_patch,green_patch,black_patch,yellow_patch,orange_patch])
 plt.show()
 
 
@@ -133,7 +139,7 @@ confusionMatrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,
 
 for row in csv:
     predicted_class = returnClass(row,meanAndSigma)
-    confusionMatrix[predicted_class][int(row[2])] += 1
+    confusionMatrix[int(row[2])][predicted_class] += 1
 
 print('\n\n')
 print('confusionmatrix:')
--- ub03/ub03_02.py
+++ ub03/ub03_02.py
@@ -9,14 +9,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as stats
 
+def s(ux_mean,uy_mean,ux,uy):
+    sSpeicher = 0
+    xSpeicher = 0    
+    for x in range(len(ux_mean)):
+        absMean = np.abs(ux_mean[x]-uy_mean[x])
+        varSum = ux[x]+uy[x]
+        s = absMean / varSum
+        if(s > sSpeicher):
+            sSpeicher = s
+            xSpeicher = x
+    return xSpeicher
+
+
 #2_a)
-mean1=(5,10)
+mean1=[5,10]
 covarianzmatrix1= [[1, 1], [1, 1]]
 
-mean2=(5,5)
+mean2=[5,5]
 covarianzmatrix2=[[2,-1],[-1,2]]
 
-mean3=(10,6)
+mean3=[10,6]
 covarianzmatrix3=[[0.1,0],[0,3]]
 
 
@@ -26,9 +39,8 @@ x3,y3 = np.random.multivariate_normal(mean3,covarianzmatrix3,100).T
 
 plt.plot(x1,y1,'x',c='red')
 plt.plot(x2,y2,'x',c='blue')
-plt.plot(x3,y3,'x',c='black')
+plt.plot(x3,y3,'x',c='green')
 
-plt.show()
 
 #2_b)
 new_covarianzmatrix1 = np.cov(x1,y1)
@@ -44,3 +56,53 @@ print("new_covarianzmatrix3:")
 print(new_covarianzmatrix3)
 print("pearson-coefficient: "+ str(stats.pearsonr(x3,y3)[0])+"\n")
 
+#2_c)
+u1_mean,u2_mean,u3_mean,u1,u2,u3 = [],[],[],[],[],[]
+
+array01=np.random.randint(-100,100,size=(1000,2))
+array02=[]
+for i in range (0, len(array01)):
+    x=np.sqrt(array01[i,0]**2+array01[i,1]**2)
+    array02.append(x) 
+array03=[]
+for j in range (0, len(array01)):
+    x=array01[j,0]/(array02[j])
+    y=array01[j,1]/(array02[j])
+    array03.append([x,y])  
+array03np = np.array(array03)    
+    
+for xyTupel in array03np:
+    u1_mean.append(np.dot(mean1,xyTupel))
+    u2_mean.append(np.dot(mean2,xyTupel))
+    u3_mean.append(np.dot(mean3,xyTupel))
+    u1.append((np.dot(np.dot(xyTupel.T,new_covarianzmatrix1),xyTupel)))
+    u2.append((np.dot(np.dot(xyTupel.T,new_covarianzmatrix2),xyTupel)))
+    u3.append((np.dot(np.dot(xyTupel.T,new_covarianzmatrix3),xyTupel)))
+
+ax = plt.subplot()
+plt.plot([(array03[s(u1_mean,u2_mean,u1,u2)][0]*-20),(array03[s(u1_mean,u2_mean,u1,u2)][0])*20],[(array03[s(u1_mean,u2_mean,u1,u2)][1])*-20,(array03[s(u1_mean,u2_mean,u1,u2)][1])*20],c='#800080')
+plt.plot([(array03[s(u1_mean,u3_mean,u1,u3)][0]*-20),(array03[s(u1_mean,u3_mean,u1,u3)][0])*20],[(array03[s(u1_mean,u3_mean,u1,u3)][1])*-20,(array03[s(u1_mean,u3_mean,u1,u3)][1])*20],c='#998000')
+plt.plot([(array03[s(u2_mean,u3_mean,u2,u3)][0]*-20),(array03[s(u2_mean,u3_mean,u2,u3)][0])*20],[(array03[s(u2_mean,u3_mean,u2,u3)][1])*-20,(array03[s(u2_mean,u3_mean,u2,u3)][1])*20],c='#008080')
+
+
+#plot set up
+red_patch = mpatches.Patch(color='red', label='class1')
+blue_patch = mpatches.Patch(color='blue', label='class2')
+black_patch = mpatches.Patch(color='green', label='class3')
+first_line_patch = mpatches.Patch(color='#800080', label='line for class 1 and 2')
+nd_line_patch = mpatches.Patch(color='#998000', label='line for class 1 and 3')
+rd_line_red_patch = mpatches.Patch(color='#008080', label='line for class 2 and 3')
+plt.legend(bbox_to_anchor=(1,1),loc=2,handles=[red_patch,blue_patch,black_patch,first_line_patch,nd_line_patch,rd_line_red_patch])
+
+ax.set_xlim([-10,15])
+ax.set_ylim([-10,15])
+
+plt.show()
+
+
+
+
+
+
+
+    
