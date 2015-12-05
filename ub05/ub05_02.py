ec68a7a1ed43ceaedd8358ae83ed98b1adea7195
Erik Z <etor@gmx.de>
2015-12-05 19:04:38 +0100

    added ub05_02.py after merging of branches master and ub05

---
+++ ub05/ub05_02.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  1 12:48:38 2015
+
+@author: erik
+"""
+#import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import itertools as it
+
+wine = pd.read_csv("./winequality-red.txt", delimiter=";",
+dtype={'names':('fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality'),
+'formats':('i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','i4','i4')},
+index_col=False,
+names=['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality'])
+#wine.head(n=5)
+qual=wine['quality']
+#print(qual)
+
+features=['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']
+
+iters= list(it.combinations(features,1))
+<<<<<<< HEAD
+print('iters',iters)
+=======
+#print('iters',iters)
+>>>>>>> master
+
+
+feat=wine[iters[0][0]]
+
+#print('feat',feat)
+
+
+lm = LinearRegression()
+
+<<<<<<< HEAD
+
+
+for j in range (0,len(feat)):
+    tmp = []
+    #dreht die Liste um 1599,1 zu 1,1599
+    feat=wine[iters[j][0]]
+    for i in feat:
+        tmp.append([i])
+    
+    lm.fit(tmp,qual)
+    prediction = lm.predict(tmp)
+    p = sum(np.square(qual-prediction))
+    plt.scatter(1,p)
+    plt.show()
+=======
+tmp = []
+for i in feat:
+    tmp.append([i])
+    
+lm.fit(tmp,qual)
+prediction = lm.predict(tmp)
+p = sum(np.square(qual-prediction))
+plt.scatter(1,p)
+plt.show()
+>>>>>>> master
+
+
+
+
+#print (lm.intercept_)
+<<<<<<< HEAD
+#print (lm.coef_)
+=======
+print (lm.coef_)
+>>>>>>> master
