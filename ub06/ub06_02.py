95529331b0c3d88b172ca3e92a874a4b6a3cd6c2
FHardow <florian.hardow@yahoo.de>
2015-12-08 14:28:09 +0100

    ub_6 percepton

--- ub06/ub06_02.py
+++ ub06/ub06_02.py
@@ -5,46 +5,87 @@ Created on Tue Dec  1 12:48:38 2015
 @author: erik
 """
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-from sklearn.lda import LDA
 
-#loadData
-fisherData = pd.read_csv("./fisher.txt",delimiter=",", dtype={'names': ('X', 'Y','theClass'),\
-'formats': ('i4','i4', 'i4')},index_col=False, names = ['x', 'y','theClass'])
-fisherData.head()
+# perceptron-algorithem with x iterations
+def perceptron(xyClass,belongingClass):
+    k = 0    
+    x = 1000000
+    w = np.zeros(len(xyClass[0])) # len of one row
+    while k < x:
+        random = np.random.randint(0,len(xyClass))
+        randomVector = xyClass[random]
+        randomBelongingClass = belongingClass[random]
+        if (randomBelongingClass):
+            w,k = perceptonPositiv(w,randomVector,k)
+        else:
+            w,k = perceptonNegativ(w,randomVector,k)
+    return w
+    
+    
+def perceptonPositiv(w,x,k):
+    if(w.dot(x) >= 0):
+        return w,k
+    else:
+        return w + x ,k+1
 
-allx=fisherData['x']
-ally=fisherData['y']
-theClass=fisherData['theClass']
-xandy = ['x','y']
-theXandY = fisherData[xandy]
+def perceptonNegativ(w,x,k):        
+    if (w.dot < 0):
+        return w,k
+    else:        
+        return w - x, k+1
+    
+    
+def normalize(vector):
+    sum = 0    
+    for x in vector:
+        sum += np.abs(x)
+    return vector / np.sqrt(sum) 
+#----------------------------- main -----------------------------
+    
+# loadData
+csv = np.genfromtxt('fisher.txt',delimiter=",")
+x0 = []
+x1 = []
+y0 = []
+y1 = []
+allClass0,allClass1 = [],[]
+allxy,allClass = [],[]
+x0_P,y0_P,x1_P,y1_P = [],[],[],[]
 
-x0=[]
-x1=[]
-y0=[]
-y1=[]
+for row in csv:
+    if (row[2]):
+        x0.append(row[0])
+        y0.append(row[1])
+        allClass0.append((row[0],row[1]))
+    else:
+        x1.append(row[0])
+        y1.append(row[1])
+        allClass1.append((row[0],row[1]))
+    allxy.append((row[0],row[1]))
+    allClass.append(row[2])
 
-for i in range(len(fisherData)):
-    if (theClass[i]==0):
-        x0.append(allx[i])
-    if (theClass[i]==1):
-      x1.append(allx[i])
+# plot all points    
+plt.plot (x0,y0,'ro')
+plt.plot (x1,y1,'^g')
 
-for j in range(len(fisherData)):
-    if (theClass[j]==0):
-        y0.append(ally[j])
-    if (theClass[j]==1):
-      y1.append(ally[j])        
+# compute and plot the mean
+mean0 = np.mean((x0,y0),axis=1)
+mean1 = np.mean((x1,y1),axis=1)
+plt.plot(mean0[0],mean0[1],'bo')
+plt.plot(mean1[0],mean1[1],'bo')
 
-fisherX=np.array(theXandY)
-fisherClass=np.array(theClass)
-clf=LDA()
-fisher=clf.fit(fisherX,fisherClass) 
-print(fisher)
-       
-#plt.plot (x0,y0,'ro')
+# covariance of both classes
+cov0 = np.cov(x0,y0)
+cov1 = np.cov(x1,y1)
 
-#plt.plot (x1,y1,'^g')
-#plt.show()
+# compute the fisher discriminant and plot it (graph of it)
+y = perceptron(allxy,allClass)
+plt.plot( (0,y[0]) , (0,y[1]) , color="cyan")
+
+# configureation of the plot, save the plot and show it
+plt.xlim(-250,250)
+plt.ylim(0,180)
+plt.savefig("LCA.png")
+plt.show()
 
